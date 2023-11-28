fb9fdcc19f8e7003cd5edaca9fe11e99fe5c20fd
Thejas p karkera <149788640+Thejas-p-karkera@users.noreply.github.com>
2023-11-28 09:47:59 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,18 @@
+package BasicsOfJava;
+
+public class Swap
+{
+	public static void main(String[] args)
+	{
+		int a=5;
+		int b=10;
+		System.out.println("Before swapping\n a = "+a+"\n b = "+b);
+		
+		a = a + b;
+        b = a - b;
+        a = a - b;
+		System.out.println("\nAfter swapping\n a = "+a+"\n b = "+b);
+		
+	}
+
+}
